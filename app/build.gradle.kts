6b653ae0528909be9932e9ac9d0cdaa8656e55b9
himanshugaur <himanshu@spare8.com>
2024-10-19 11:04:42 +0530

    Initial Project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.dagger)
+    alias(libs.plugins.compose.compiler)
     kotlin("kapt")
 }
 
